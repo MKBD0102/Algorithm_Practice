6eab4939aa0daa83408503aa3c92211daa4648f8
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-04 10:49:34 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.py
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -1,4 +1,5 @@
 def solution(emergency):
     em_order = sorted(emergency,reverse=True)
-    em_dict = {k:(em_order.index(k)+1) for k in em_order}
+    # em_dict = {k:(em_order.index(k)+1) for k in em_order}
+    em_dict = {k:v+1 for v,k in enumerate(em_order)} # index 안쓰는 방법
     return [em_dict.get(k) for k in emergency]
