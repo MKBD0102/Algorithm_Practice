4b9a1ee6752be966864a5bb940047be657d0f03c
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-04 11:07:35 +0900

    [level unrated] Title: 모스부호 (1), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.py
+++ 프로그래머스/unrated/120838. 모스부호 （1）/모스부호 （1）.py
@@ -7,4 +7,7 @@ def solution(letter):
     '-.--':'y','--..':'z'
 }
     codes = letter.split(" ")
-    return ''.join([morse.get(k) for k in codes])
+    # return ''.join([morse.get(k) for k in codes])
+    # 리스트 컴프리헨션 대신 제너레이터 표현식 사용
+    morse_generator = (morse.get(k) for k in codes)
+    return ''.join(morse_generator)
