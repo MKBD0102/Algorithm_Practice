a4ad017e1551567751db85533e19de964eb2eb7c
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-05 18:24:19 +0900

    [level unrated] Title: 합성수 찾기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.py
+++ 프로그래머스/unrated/120846. 합성수 찾기/합성수 찾기.py
@@ -10,3 +10,18 @@ def solution(n):
         if divnum(i) >= 3:
             cnt += 1
     return cnt
+'''
+def solution(n):
+    output = 0
+# 1,2,3은 약수의 개수 2 이하 제외
+    for i in range(4, n + 1): 
+    
+# 약수가 대칭구조이므로 제곱근까지만 확인하여 불필요한 반복 제외 
+        for j in range(2, int(i ** 0.5) + 1):  
+
+# 1과 자기 자신도 약수에 포함되므로 그 이외의 약수가 존재하면 3개 이상임 따라서 반복문을 빠져나와 불필요한 반복 제외
+            if i % j == 0:
+                output += 1 
+                break
+    return output
+'''
