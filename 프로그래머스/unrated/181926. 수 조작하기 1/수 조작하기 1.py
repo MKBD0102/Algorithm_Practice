077c4ae26cd6b67e54c8467e20114603f57e5579
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-06 23:57:11 +0900

    [level unrated] Title: 수 조작하기 1, Time: 0.23 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.py
@@ -1,2 +1,12 @@
 def solution(n, control):
     return n + 1 * control.count('w') - 1 * control.count('s') + 10 * control.count('d') - 10 * control.count('a')
+
+'''
+# dictionary 사용 방법
+# dict와 zip으로 대응하는 계산 값 저장
+dic = dict(zip(['w','s','d','a'],[1,-1,10,-10]))
+# dictionary에서 문자열을 순회하며 키에 해당하는 값을 반환, sum 계산
+return n + sum([dic[key] for key in control])
+
+# 위 코드로 본인 작성 코드에서 불필요한 반복(찾는 문자마다 매번 문자열 순회) 제거
+'''
