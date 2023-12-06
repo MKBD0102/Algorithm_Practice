02efdcc5008be1f40067824b540af4ca8627c364
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-07 01:16:21 +0900

    [level unrated] Title: 배열 만들기 2, Time: 15.40 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -6,3 +6,15 @@ def solution(l, r):
                 answer.append(n)
         
     return [-1] if answer == [] else answer
+
+'''
+def solution(l, r):
+    answer = []
+    for num in range(l, r + 1):
+# 문자열을 set()에 넣으면 각 문자가 set의 원소가 됨 ex) set('hello') -> {'h','e','l','o'}
+# (not 차집합)이 공집합이면 True, 공집합이 아니면 False 반환됨    
+        if not set(str(num)) - set(['0', '5']):
+            answer.append(num)
+    return answer if answer else [-1]
+
+'''
