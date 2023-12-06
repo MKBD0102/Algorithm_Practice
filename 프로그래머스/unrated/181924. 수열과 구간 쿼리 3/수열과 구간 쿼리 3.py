4adb149d0c14c86e734e42e140690b6963bf6d58
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-07 00:26:48 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.py
@@ -6,3 +6,12 @@ def solution(arr, queries):
         arr[i] = num_j
         arr[j] = num_i
     return arr
+
+'''
+def solution(arr, queries):
+    for a,b in queries:
+    # 값을 따로 저장해두지 않고 바로 교환 가능
+        arr[a],arr[b]=arr[b],arr[a]
+    return arr
+
+'''
