a024b19b7e954877202e3bd2d43fdb5e2c0f849c
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-10 16:20:42 +0900

    Update 접미사인지 확인하기.py

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,5 +1,11 @@
 def solution(my_string, is_suffix):
-    return 1 if my_string[-len(is_suffix):] == is_suffix else 0
+    if len(my_string) >= len(is_suffix):
+        if my_string[-len(is_suffix):] == is_suffix:
+            return 1
+        else:
+            return 0
+    else:
+        return 0
 
 '''
 # 코드 줄이기
