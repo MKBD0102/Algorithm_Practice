5a5dce622410c853496a8de4e1f1b8192db2c7af
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-10 16:08:59 +0900

    [level unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.py
@@ -4,3 +4,8 @@ def solution(my_strings, parts):
         s,e = parts[i]
         answer += my_strings[i][s:e+1]
     return answer
+
+'''
+# join 활용
+return ''.join([my_strings[i][parts[i][0]:parts[i][1]+1] for i in range(len(parts))])
+'''
