00973a02b2b7c2b87b58aebfef01fe7491da9edb
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-10 15:57:16 +0900

    [level unrated] Title: 배열 만들기 5, Time: 1.69 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.py
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.py
@@ -5,3 +5,8 @@ def solution(intStrs, k, s, l):
         if num > k:
             ret.append(num)
     return ret
+
+'''
+# 리스트 컴프리헨션
+return [int(c[s:s+l]) for c in intStrs if int(c[s:s+l]) > k]
+'''
