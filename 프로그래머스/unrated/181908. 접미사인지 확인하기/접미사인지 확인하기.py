c3ab14f04daa59c0e8b9951572e5439fd1f001a4
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-10 16:17:03 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.py
@@ -1,8 +1,7 @@
 def solution(my_string, is_suffix):
-    if len(my_string) >= len(is_suffix):
-        if my_string[-len(is_suffix):] == is_suffix:
-            return 1
-        else:
-            return 0
-    else:
-        return 0
+    return 1 if my_string[-len(is_suffix):] == is_suffix else 0
+
+'''
+# 코드 줄이기
+return 1 if my_string[-len(is_suffix):] == is_suffix else 0
+'''
