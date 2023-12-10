081cd1d9f0f85bae7c37f8cd586dc2ddab05b142
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-10 15:39:13 +0900

    [level unrated] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.py
@@ -20,3 +20,42 @@ def solution(a, b, c, d):
                 get_num.remove(n)
                 get_num = list(get_num)
                 return (10 * n + get_num[0]) ** 2
+            
+'''
+# 시간 복잡도는 같음
+# 중복되는 코드 없는 풀이
+def solution(a, b, c, d):
+    answer = 0
+    if a==b==c==d:
+        answer=1111*a
+    elif a==b==c:
+        answer=(10*a+d)**2
+    elif a==b==d:
+        answer=(10*a+c)**2
+    elif a==c==d: 
+        answer=(10*a+b)**2
+    elif b==c==d:
+        answer=(10*d+a)**2
+    elif a==b and c==d:
+        answer=(a+c)*abs(a-c)
+    elif a==c and b==d:
+        answer=(a+b)*abs(a-b)
+    elif a==d and b==c:
+        answer=(a+b)*abs(a-b)
+    elif a==b:
+        answer=c*d
+    elif a==c:
+        answer=b*d
+    elif a==d:
+        answer=b*c
+    elif b==c:
+        answer=a*d
+    elif b==d:
+        answer=a*c
+    elif c==d:
+        answer=a*b
+    else:
+        answer=min(a,b,c,d)
+
+    return answer
+'''
