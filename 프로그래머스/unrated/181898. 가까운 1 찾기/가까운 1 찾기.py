a07dea80da3c82e6aec03c28f24457b683ca6968
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-11 23:35:44 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -2,5 +2,18 @@ def solution(arr, idx):
     for i in range(idx,len(arr)):
         if arr[i] == 1:
             return i
-        elif arr[i] != 1 and i == len(arr)-1:
-                return -1
+        elif i == len(arr)-1:
+            return -1
+        
+'''
+# 반복문과 조건문 중첩 X -> try except 구문 사용
+def solution(arr, idx):
+    answer = 0
+    try:
+        answer = arr.index(1, idx)  
+                # array.index(x, start, stop) 리스트[start:stop]에서 x의 인덱스 반환
+    except:
+        answer = -1
+
+    return answer
+'''
