6585f0231ac8634c52d5b37be57528e0fca35515
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-11 23:24:21 +0900

    [level unrated] Title: 글자 지우기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
@@ -2,3 +2,8 @@ def solution(my_string, indices):
     for d in sorted(indices,reverse=True):
         my_string = my_string[:d]+my_string[d+1:]
     return my_string
+
+'''
+# 리스트 컴프리헨션 사용 & 원본 변경 X
+return ''.join([v for i, v in enumerate(my_string) if i not in indices])
+'''
