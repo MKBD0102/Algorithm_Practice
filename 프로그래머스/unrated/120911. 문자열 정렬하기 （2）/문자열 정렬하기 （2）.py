431c44b47d7c08568353cb3f3f46e9cbc1df2e19
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-11 23:40:58 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -1,2 +1,7 @@
 def solution(my_string):
     return ''.join(sorted([s.lower() for s in my_string]))
+
+'''
+# string 형식도 sorted() 가능 -> 단 리스트 형식으로 반환
+return ''.join(sorted(my_string.lower()))
+'''
