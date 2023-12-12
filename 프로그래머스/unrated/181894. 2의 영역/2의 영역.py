0d5deafa25b211b31236303eca187e73bb66368d
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-12 23:25:35 +0900

    [level unrated] Title: 2의 영역, Time: 3.67 ms, Memory: 15.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -4,3 +4,13 @@ def solution(arr):
         if n == 2:
             idx.append(i)
     return arr[min(idx):max(idx)+1] if idx else [-1]
+
+'''
+# min, max 안쓰는 방법
+def solution(arr):
+    if 2 not in arr:
+        return [-1]
+    return arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
+# index()로 최초의 2의 인덱스 가져오고, arr[::-1].index()로 뒤에서 부터 확인하여 가장 마지막의 2의 인덱스 가져옴
+# len(arr) - arr[::-1].index(): arr[::-1]로 인덱스가 뒤집어 졌으므로 리스트 길이에서 빼서 원래 인덱스 찾음
+'''
