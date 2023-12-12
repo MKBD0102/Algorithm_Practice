fa9ff3da388b6a55427fc31557ec02cfd68a1b8a
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-13 00:42:56 +0900

    [level unrated] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/unrated/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -3,3 +3,8 @@ def solution(dots):
     l = list(set([x1, x2, x3, x4]))
     h = list(set([y1, y2, y3, y4]))
     return  abs(l[0] - l[1])*abs(h[0] - h[1])
+
+'''
+# 집합과 절댓값 함수 사용 안하는 방식
+return (max(dots[0]) - min(dots[0])) * (max(dots[1]) - min(dots[1]))
+'''
