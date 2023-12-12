cb5949abb163c67ab091d12a40798aa6c2f16118
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-13 00:27:05 +0900

    [level unrated] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/unrated/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -10,4 +10,14 @@ def solution(str_list):
             return str_list[:str_list.index("l")]
         else:
             return str_list[str_list.index("r")+1:]
-    
+
+'''
+# 우선 오는 문자 확인하는 방식
+def solution(str_list):
+    for s in str_list:
+        if s == "l":
+            return str_list[:str_list.index("l")]
+        elif s == "r":
+            return str_list[str_list.index("r")+1:]
+    return []
+'''
