ad4008eb4afe1942aa788d9e10d77bf40fb6cd38
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-12 23:42:37 +0900

    [level unrated] Title: 배열 조각하기, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
+++ 프로그래머스/unrated/181893. 배열 조각하기/배열 조각하기.py
@@ -5,3 +5,14 @@ def solution(arr, query):
         else:
             arr = arr[query[i]:]
     return arr
+
+'''
+# 인덱스 색인 과정 제외
+def solution(arr, query):
+    for i, n in enumerate(query):
+        if i % 2 == 0:
+            arr = arr[:j+1]
+        else:
+            arr = arr[j:]
+    return arr
+'''
