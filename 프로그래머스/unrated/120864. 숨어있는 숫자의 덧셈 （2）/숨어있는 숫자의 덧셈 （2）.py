76f7e4c64a75f586afa66c82ab05d1aa81f4073e
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-16 00:16:05 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,3 +1,3 @@
 def solution(my_string):
-    a = ''.join([(' ' if s.isalpha() else s) for s in my_string])
+    a = ''.join((' ' if s.isalpha() else s) for s in my_string).strip()
     return sum(int(n) for n in a.split(' ') if n != '')
