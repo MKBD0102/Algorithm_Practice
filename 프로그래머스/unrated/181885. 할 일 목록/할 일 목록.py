7a1dada9359ae3f43fa40b544df3b7e216c6f45d
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-15 09:38:31 +0900

    [level unrated] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.py
@@ -1,2 +1,7 @@
 def solution(todo_list, finished):
     return [todo_list[i] for i in range(len(finished)) if not finished[i]]
+
+'''
+# enumerate 사용
+return [s for i,s in enumerate(todo_list) if not finished[i]]
+'''
