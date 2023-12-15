b423de2dfb4c5a73ad90ce25cb35bf3b593598f3
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-15 22:30:34 +0900

    [level unrated] Title: 수열과 구간 쿼리 1, Time: 6.52 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
+++ 프로그래머스/unrated/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.py
@@ -3,3 +3,12 @@ def solution(arr, queries):
         for i in range(s,e+1):
             arr[i] += 1
     return arr
+
+
+'''
+# enumerate -> index와 value 한 번에
+
+for s, e in queries:
+        arr = [n+1 if s <= i <= e else n for i, n in enumerate(arr)]
+return arr
+'''
