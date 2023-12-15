f0d45541302e68a709355f23f2f186a0c25c4b6d
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-16 00:40:05 +0900

    [level unrated] Title: 삼각형의 완성조건 (2), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/unrated/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,2 +1,18 @@
 def solution(sides):
     return len(list(range(max(sides) - min(sides)+1,max(sides))) + list(range(max(sides),sum(sides))))
+
+'''
+# # 가장 길지 않을 조건: max(sides) < res < sum(sides)
+# # 구간 길이 = 만족하는 정수 개수
+# sum(sides) - max(sides) - 1
+
+# # 가장 길 조건: max(sides) - min(sides) < res < max(sides)
+# max(sides) - max(sides) + min(sides) - 1
+# -> min(sides) - 1
+
+# # 가능한 총 정수 개수
+# sum(sides) - max(sides) - 2 + min(sides)
+
+# 따라서
+return sum(sides) - max(sides) + min(sides) - 2
+'''
