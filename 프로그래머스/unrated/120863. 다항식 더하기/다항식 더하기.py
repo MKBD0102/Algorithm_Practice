78bc45913aacafc34b1925188bbab9566a243fbf
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-16 01:19:55 +0900

    [level unrated] Title: 다항식 더하기, Time: 0.02 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.py
+++ 프로그래머스/unrated/120863. 다항식 더하기/다항식 더하기.py
@@ -9,8 +9,10 @@ def solution(polynomial):
             x_sum += 1
         else:
             x_sum += int(term.replace('x',''))
-    if x_sum == 1:
-        x_sum = ''
-    return f'{x_sum}x + {c_sum}' if c_sum != 0 and x_sum != 0 else \
-            f'{x_sum}x' if c_sum == 0 and x_sum != 0 else \
-            f'{c_sum}' if x_sum == 0 else None
+    if x_sum == 0:
+        return f'{c_sum}'
+    elif x_sum == 1:
+        return f'x + {c_sum}' if c_sum != 0 else f'x'
+    else:
+        return f'{x_sum}x + {c_sum}' if c_sum != 0 else f'{x_sum}x'
+            
