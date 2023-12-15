bd5c8189220f4f345f9d51c09c1414942e21ee2e
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-16 02:25:13 +0900

    [level unrated] Title: A 강조하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.py
@@ -1,2 +1,7 @@
 def solution(myString):
     return ''.join([s.lower() if s not in ["A","a"] else s.upper() for s in myString])
+
+'''
+# 모두 소문자로 바꾼 뒤 a만 A로 바꾸는 법
+return myString.lower().replac('a','A')
+'''
