ee9e7557d715771183cc3817347770b8858932e1
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-16 03:02:35 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 7.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.py
@@ -1,2 +1,7 @@
 def solution(arr, k):
     return list(map(lambda x:x*k,arr)) if k % 2 == 1 else list(map(lambda x:x+k,arr))
+
+'''
+# 리스트 컴프리헨션
+return [n * k if k % 2 == 1 else n + k for n in arr]
+'''
