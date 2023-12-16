d5ecf0fc5d12618dcc8d2620ebdac05dc7861727
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-16 10:47:20 +0900

    [level unrated] Title: 0 떼기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.py
@@ -2,3 +2,8 @@ def solution(n_str):
     for i, n in enumerate(n_str):
         if n != '0':
             return ''.join(list(n_str)[i:])
+        
+'''
+# lstrip() -> 왼쪽부터 자르기
+return n_str.lstrip('0')
+'''
