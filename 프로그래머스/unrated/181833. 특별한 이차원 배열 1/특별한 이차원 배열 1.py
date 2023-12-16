793d4de982f593b705e907d0336b843cceb7b313
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-16 15:20:00 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.28 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,2 +1,10 @@
 def solution(n):
     return [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+
+'''
+# 
+def solution(n):
+    answer=[[0]*n for i in range(n)]
+    for i in range(n): answer[i][i]=1
+    return answer
+'''
