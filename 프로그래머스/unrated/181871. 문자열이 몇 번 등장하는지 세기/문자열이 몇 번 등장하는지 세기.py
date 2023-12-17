ae54d8f833fe1247c7ae7e11345385902226e5a6
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-17 14:54:12 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -4,3 +4,15 @@ def solution(myString, pat):
         if myString[i:].find(pat) == 0:
             cnt += 1
     return cnt
+
+
+'''
+# startswith 함수 사용
+def solution(myString, pat):
+    cnt = 0
+    for i, x in enumerate(myString) :
+        if myString[i:].startswith(pat) :  # startswith() return True/False
+            cnt += 1
+    return cnt
+
+'''
