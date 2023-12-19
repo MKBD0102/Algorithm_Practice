7ca8f8cb7f94501d59f46e30ca4392584f5305f8
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-20 00:38:21 +0900

    [level unrated] Title: 그림 확대, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
@@ -4,3 +4,13 @@ def solution(picture, k):
         string = ''.join([s*k for s in string])
         ans += [string for i in range(k)]
     return ans
+
+'''
+# lambda 함수 사용
+return lambda picture, k: [''.join([s*k for s in picture[x//k]]) for x in range(len(picture)*k)]
+
+# for x in range(len(picture)*k) -> 세로로 k배 늘린 만큼 리스트 길이 늘이기
+# for s in picture[x//k] -> x는 원래 길이의 k배의 범위를 탐색하므로, 원래 리스트 요소의 인덱스를 찾기 위해 x를 k로 나눈 몫을 찾음.
+# ''.join([s*k for s in picture[x//k]]) -> 세로로 k배 늘인 범위를 탐색하는 x, x//k로 인해 같은 원소에 대한 join을 k번 수행하게 됨
+
+'''
