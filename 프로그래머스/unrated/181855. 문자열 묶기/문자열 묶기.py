1e0747f2ec90db9af7a7abb7ec14c70ad78b3e31
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-19 23:38:55 +0900

    [level unrated] Title: 문자열 묶기, Time: 26.94 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
@@ -6,3 +6,13 @@ def solution(strArr):
         else:
             answer[len(s)] += 1
     return max(answer.values())
+
+'''
+# get(key, key 없으면 넣을 디폴트 값) (-> key에 해당하는 값) 함수 사용
+def solution(strArr):
+    answer = {}
+    for s in strArr:
+        answer[len(s)] = answer.get(len(s),0) + 1
+    return max(answer.values())
+
+'''
