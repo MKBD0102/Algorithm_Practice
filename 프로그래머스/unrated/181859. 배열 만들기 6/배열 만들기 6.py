0773e9d14d73fc7ca148c323abcc974b991b0bf1
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-19 22:26:01 +0900

    [level unrated] Title: 배열 만들기 6, Time: 33.07 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.py
@@ -2,12 +2,8 @@ def solution(arr):
     stk = []
     i = 0
     while i < len(arr):
-        if stk:
-            if stk[-1] == arr[i]:
+        if stk and stk[-1] == arr[i]:
                 stk.pop()
-                i += 1
-            else:
-                stk.append(arr[i])
                 i += 1
         else:
             stk.append(arr[i])
