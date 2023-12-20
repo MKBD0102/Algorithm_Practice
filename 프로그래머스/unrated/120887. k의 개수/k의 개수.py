959eb3704b930ef5f458f41aca179de8459a0b53
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-21 00:10:08 +0900

    [level unrated] Title: k의 개수, Time: 9.59 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120887. k의 개수/k의 개수.py
+++ 프로그래머스/unrated/120887. k의 개수/k의 개수.py
@@ -1,5 +1,12 @@
+
 def solution(i, j, k):
     cnt = 0
     for n in range(i, j+1):
         cnt += str(n).count(str(k))
     return cnt
+
+'''
+# 리스트 컴프리헨션 사용
+def solution(i, j, k):
+    return sum([ str(i).count(str(k)) for i in range(i,j+1)])
+'''
