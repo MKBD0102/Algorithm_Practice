11c88ef0dada0f84cb956681e9bb80ead97f2e4d
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-20 22:07:26 +0900

    [level unrated] Title: 등수 매기기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/unrated/120882. 등수 매기기/등수 매기기.py
@@ -2,3 +2,14 @@ def solution(score):
     avg = [(x+y)/2 for x, y in score]
     sorted_avg = sorted(avg, reverse=True)
     return [sorted_avg.index(a)+1 for a in avg]
+
+'''
+# dict 활용 (굳이 평균을 다 구할 필요 없이 sum만 해도 비교 가능)
+def solution(score):
+    rank = sorted([sum(s) / 2 for s in score], reverse=True)
+    rankDict = {}
+    for i, r in enumerate(rank):
+        if r not in rankDict.keys():
+            rankDict[r] = i + 1
+    return [rankDict[sum(s) / 2] for s in score]
+'''
