882cfbc6752fd18ba829a466a8e7733099aceb11
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-21 16:57:42 +0900

    [level unrated] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/unrated/120921. 문자열 밀기/문자열 밀기.py
@@ -3,3 +3,10 @@ def solution(A, B):
         if B == A[-i:]+A[:-i]:
             return i
     return -1
+
+'''
+# lambda 사용
+return lambda A,B : (B*2).find(A)
+-> find(A): A가 처음 등장하는 위치 index = 회전 최소 횟수,
+만약 A가 없다면 find는 -1을 반환
+'''
