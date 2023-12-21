953f35d9a88f2344efc1a5a32b77eaee21bdb42d
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-21 23:27:14 +0900

    [level 1] Title: 약수의 합, Time: 0.16 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,2 +1,7 @@
 def solution(n):
     return sum([x for x in range(1,n+1) if n % x == 0])
+
+'''
+# 자기 자신을 제외한 가장 큰 약수는 자신을 2로 나눈 수보다 작거나 같음
+return num + sum([x for x in range(1,(n//2)+1) if n % x == 0])
+'''
