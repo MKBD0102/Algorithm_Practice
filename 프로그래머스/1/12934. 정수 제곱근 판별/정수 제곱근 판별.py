2dd9ceb0a831e665a7dcd346e6ad76ed81c835a5
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-22 00:03:00 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.py
@@ -3,3 +3,12 @@ def solution(n):
         if n / i == i:
             return (i+1) ** 2
     return -1
+
+'''
+# 제곱근 수식 활용
+def solution(n):
+    x = n ** (0.5)
+    if x % 1 == 0:  # 정수인지 확인
+        return (x+1) ** 2
+    return -1
+'''
