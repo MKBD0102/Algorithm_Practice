e4c568dd0c704ba758b052d39a92f45be7174a81
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-25 15:04:55 +0900

    [level 1] Title: 행렬의 덧셈, Time: 13.27 ms, Memory: 22.8 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,2 +1,9 @@
 def solution(arr1, arr2):
     return [[ x+y for x,y in zip(X,Y)] for X,Y in zip(arr1,arr2)]
+
+'''
+# map 함수 활용, * -> 언패킹
+def solutin(arr1, arr2):
+    return [list(map(sum, zip(*x))) for x in zip(arr1, arr2)]
+
+'''
