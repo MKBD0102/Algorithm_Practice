e15794fdd92dde049b6a0d91c24f470e283fbef3
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-25 14:32:54 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,3 +1,8 @@
 def solution(price, money, count):
     shortage = price * sum(range(count+1)) - money
     return shortage if shortage > 0 else 0
+
+'''
+# max 함수 사용, count*(count+1)//2: 1부터 count까지의 자연수 합
+return max(0,price * count*(count+1)//2 - money)
+'''
