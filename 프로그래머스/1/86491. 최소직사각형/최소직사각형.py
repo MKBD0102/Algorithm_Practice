293335f30ed5ffcdfcde98a916c1e716679de187
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-27 23:06:01 +0900

    [level 1] Title: 최소직사각형, Time: 7.51 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -5,4 +5,10 @@ def solution(sizes):
     max_h = max(size[1] for size in rotated)
     
     return max_w * max_h
+
+'''
+# 모든 명함 회전해서 명함별 긴 모서리는 가로에, 명함별 짧은 모서리는 세로에 배치
+# 긴 모서리들 중 가장 긴 모서리가 가로의 최대 길이, 짧은 모서리들 중 가장 긴 모서리가 세로의최대 길이
+return max(max(size) for size in sizes)*max(min(size) for size in sizes)
+'''
     
