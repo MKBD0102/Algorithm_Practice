a484a538d27f10c08f92c1d4fb2b96fb38a63284
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-27 18:13:58 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,18 +1,29 @@
 def solution(s):
-    i = j = 0
+    j = 0
     res = ''
-    while i < len(s):
+    for i in range(len(s)):
         if s[i] == ' ':
             res += s[i]
-            i += 1
             j = 0
             
         elif j % 2 == 0:
             res += s[i].upper()
-            i += 1
             j += 1
         elif j % 2 != 0:
             res += s[i].lower()
-            i += 1
             j += 1
+#     while i < len(s):
+#         if s[i] == ' ':
+#             res += s[i]
+#             i += 1
+#             j = 0
+            
+#         elif j % 2 == 0:
+#             res += s[i].upper()
+#             i += 1
+#             j += 1
+#         elif j % 2 != 0:
+#             res += s[i].lower()
+#             i += 1
+#             j += 1
     return res
