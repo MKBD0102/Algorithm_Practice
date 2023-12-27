540b2130d63d11f8eaf3e79ca4bac469bb2848c3
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-27 14:51:47 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,3 +1,4 @@
+''''
 def solution(d, budget):
     res = 0
     d = sorted(d)
@@ -5,6 +6,20 @@ def solution(d, budget):
         if sum(d[:res+1]) > budget:
             return res
         else:
+            res += 1        
+    return res
+
+'''
+# 리스트 슬라이싱 -> 오버헤드 위험
+def solution(d, budget):
+    hap = 0
+    res = 0
+    d.sort()
+
+    for i in range(len(d)):
+        hap += d[i]
+        if hap > budget:
+            break
+        else:
             res += 1
-        
     return res
