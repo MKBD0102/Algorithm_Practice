8e31015ff3b52dae42757c3c03ec5210e136c988
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-27 14:45:44 +0900

    [level 1] Title: 예산, Time: 0.10 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.py
+++ 프로그래머스/1/12982. 예산/예산.py
@@ -1,7 +1,8 @@
 def solution(d, budget):
     res = 0
+    d = sorted(d)
     while res < len(d):
-        if sum(sorted(d)[:res+1]) > budget:
+        if sum(d[:res+1]) > budget:
             return res
         else:
             res += 1
