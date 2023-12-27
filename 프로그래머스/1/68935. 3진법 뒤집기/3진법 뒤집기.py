364a44ee42421d9712b497d02b9bf4000eaf860d
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-27 11:05:01 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -9,9 +9,27 @@ def solution(n):
     
     def ttod(string):
         number = 0
-        string[::-1]
+        reversed(string)
         for i,s in enumerate(string):
             number += (3 ** i) * int(s)
         return number
     
     return ttod(dtot(n))
+
+'''
+# 재귀 함수 사용 X, 문자열 뒤집기 X
+def solution(n):
+    ternay = []
+    answer = 0
+    power = 0
+    
+    while n > 0:
+        ternay.append(n % 3)
+        n = n // 3
+    
+    for i in reversed(ternay):
+        answer += i * (3 ** power)
+        power += 1
+    
+    return answer
+'''
