024318d48bf4868c948bf1283ddc1c9260a6ed64
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-27 22:23:33 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,4 +1,3 @@
-'''
 def solution(number):
     n = len(number)
     comb = []
@@ -23,4 +22,4 @@ def solution(number):
     if n < 3:
         return cnt
     return sum(1 for i in range(n - 2) for j in range(i + 1, n - 1) for k in range(j + 1, n) if number[i]+number[j]+number[k] == 0)
-
+'''
