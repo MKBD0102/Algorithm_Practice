cd0f7915c7d15cee0e15ab5d81271d3860dca28d
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2023-12-31 14:37:25 +0900

    [level 1] Title: 시저 암호, Time: 2.81 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.py
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.py
@@ -9,3 +9,18 @@ def solution(s, n):
             a = up_letter[(up_letter.index(a) + n) % len(up_letter)]
         ans += a
     return ans
+
+'''
+# 리스트 생성 X
+# 문자도 부등호 비교 가능
+def solution(s, n):
+    ans = ''
+    for a in s:
+        if a >= 'a' and a <= 'z':
+            ans += chr((ord(a) - ord('a') + n) % 26 + ord('a'))
+        elif a >= 'A' and a <= 'Z':
+            ans += chr((ord(a) - ord('A') + n) % 26 + ord('A'))
+        else:
+            ans += a
+    return ans
+'''
