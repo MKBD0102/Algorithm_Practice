cf724e86510f4f09a4bfb86f41a693e998bca9cc
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-02 22:50:06 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,8 +1,7 @@
 def solution(food):
-    ans = []
+    ans = ''
     for i in range(1,len(food)):
         if food[i] // 2 > 0:
-            ans += [str(i)] * (food[i] // 2)
-    ans = ans+['0']+ans[::-1]
-    if len(ans) >= 3:
-        return ''.join(ans)
+            ans += str(i) * (food[i] // 2)
+    if len(ans) * 2 + 1 >= 3:
+        return ans+'0'+ans[::-1]
