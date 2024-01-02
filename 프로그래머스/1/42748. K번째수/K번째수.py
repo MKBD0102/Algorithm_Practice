98df612578e6bb7d8cc516d4312b6df83b8acac3
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-02 22:22:25 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -4,3 +4,8 @@ def solution(array, commands):
         sorted_array = sorted(array[i-1:j])
         ans.append(sorted_array[k-1])
     return ans
+
+'''
+# map 함수 활용
+    return list(map(lambda x: sorted(array[x[0] - 1:x[1]])[x[2]-1], commands))
+'''
