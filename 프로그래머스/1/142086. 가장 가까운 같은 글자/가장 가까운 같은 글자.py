35f52cdcc26fabf9d2b7c11f5b4a44189382ec6f
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-02 16:27:53 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 67.80 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -9,3 +9,18 @@ def solution(s):
                     ans[i] = j+1
                     break
     return ans
+
+'''
+# 딕셔너리 활용
+def solution(s):
+    ans = []
+    dic = {}
+    for i, a in enumerate(s):
+        if a not in dic:
+            ans.append(-1)
+        else:
+            ans.append(i - dic[a])
+        dic[a] = i
+
+    return ans
+'''
