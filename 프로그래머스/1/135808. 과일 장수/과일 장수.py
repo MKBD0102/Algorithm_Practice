8db06b2291a125ef55b67c3c44c524ade36c04a7
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-03 01:15:17 +0900

    [level 1] Title: 과일 장수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -2,3 +2,17 @@ def solution(k, m, score):
     sorted_score = sorted(score, reverse=True)
     min_scores = sorted_score[m-1::m]
     return sum(map(lambda x: x*m,min_scores))
+
+'''
+def solution(k, m, score):
+    answer = 0
+    score.sort(reverse=True)
+    apple_box = []
+    for i in range(0, len(score), m):
+        apple_box.append(score[i:i+m])
+    for apple in apple_box:
+        if len(apple) == m:
+            answer += min(apple) * m
+
+    return answer
+'''
