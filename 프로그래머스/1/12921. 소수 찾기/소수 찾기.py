6e48a1f08a853d4604a2a3786761613bbf7ea402
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-04 13:30:23 +0900

    [level 1] Title: 소수 찾기, Time: 3072.29 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -6,3 +6,15 @@ def solution(n):
         return True
     
     return sum(is_prime(x) for x in range(2,n+1))
+
+'''
+# 집합과 배수 활용
+# set(range(2*i,n+1,i)): i를 제외한 i의 배수 집합
+def solution(n):
+    num=set(range(2,n+1))
+
+    for i in range(2,n+1):
+        if i in num:
+            num-=set(range(2*i,n+1,i))
+    return len(num)
+'''
