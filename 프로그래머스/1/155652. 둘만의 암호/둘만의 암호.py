6484d41bfeb496b64b52e528ff1528fe188c32fb
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-08 23:52:53 +0900

    [level 1] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.py
@@ -1,7 +1,18 @@
+'''
 def solution(s, skip, index):
     alphabet_list = [chr(n) for n in range(ord('a'), ord('z') + 1)]
     for r in skip:
         alphabet_list.remove(r)
+    ans = ''
+    for a in s:
+        ans += alphabet_list[(alphabet_list.index(a) + index) % len(alphabet_list)]
+    return ans
+
+'''
+# remove 여러 번 -> 성능 저하
+# 처음 리스트 만들 때 skip 제외하고 생성
+def solution(s, skip, index):
+    alphabet_list = [chr(n) for n in range(ord('a'), ord('z') + 1) if chr(n) not in skip]    
     ans = ''
     for a in s:
         ans += alphabet_list[(alphabet_list.index(a) + index) % len(alphabet_list)]
