df239baaada0c4c3e322621bf5763a0adcb34b2b
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-10 01:16:52 +0900

    [level 1] Title: 공원 산책, Time: 0.08 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,4 +1,6 @@
+'''
 def solution(park, routes):
+    
     for i, row in enumerate(park):
         if 'S' in row:
             res = [i, row.find('S')]
@@ -41,3 +43,43 @@ def solution(park, routes):
                 res[1] += n
                 
     return res
+
+'''
+# 다른 풀이 -> 이동 패턴 -> dict
+def solution(park, routes):
+    dx = {'N':-1, 'S':1, 'E':0, 'W': 0}
+    dy = {'N': 0, 'S':0, 'E':1, 'W':-1}
+    
+    x, y = -1, -1
+    
+    H = len(park)
+    W = len(park[0])
+    
+    for i in range(H):
+        for j in range(W):
+            if park[i][j] == 'S':
+                x = i
+                y = j
+                break
+        if x != -1:
+            break
+    
+    for route in routes:
+        op, n = route.split()
+        n = int(n)
+        
+        isFalse = False
+        for i in range(1, n+1):
+            nx, ny = x+dx[op]*i, y+dy[op]*i
+            if nx < 0 or ny < 0 or nx > (H-1) or ny > (W-1):
+                isFalse = True
+                break
+            if park[nx][ny] == 'X':
+                isFalse = True
+                break
+        if isFalse:
+            continue
+        x += dx[op]*n
+        y += dy[op]*n
+                
+    return [x,y]
