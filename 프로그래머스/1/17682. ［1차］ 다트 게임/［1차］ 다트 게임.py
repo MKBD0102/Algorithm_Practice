7cdeb01680475da1c2960edc31590f2b7f4eef0e
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-30 20:07:37 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.25 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -0,0 +1,31 @@
+import re
+
+def solution(dartResult):
+    pattern = re.compile('(\d+)([SDT])([*#]?)')
+    times = pattern.findall(dartResult)
+    
+    scores = []
+    for i, time in enumerate(times):
+        
+        num, area, option = time
+        num = int(num)
+        
+        if area == "S":
+            num = num**1
+        elif area == "D":
+            num = num**2
+        elif area == "T":
+            num = num**3
+        
+        if option == "*":
+            try:
+                scores[i-1] = scores[i-1]*2
+            except:
+                pass
+            num *= 2
+        elif option == "#":
+            num = -num
+        
+        scores.append(num)
+        
+    return sum(scores)
