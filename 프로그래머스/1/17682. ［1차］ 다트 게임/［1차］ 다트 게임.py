16d28dffb164901117d0f644f1bf6a35125ab367
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-30 20:41:24 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.23 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -27,3 +27,18 @@ def solution(dartResult):
         scores.append(num)
         
     return sum(scores)
+
+'''
+# 조건문 대신 딕셔너리로 SDT 경우와 옵션의 경우를 저장 후 계산
+def solution(dartResult):
+    bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+    option = {'' : 1, '*' : 2, '#' : -1}
+    p = re.compile('(\d+)([SDT])([*#]?)')
+    dart = p.findall(dartResult)
+    for i in range(len(dart)):
+        if dart[i][2] == '*' and i > 0:
+            dart[i-1] *= 2
+        dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+
+    return sum(dart)
+'''
