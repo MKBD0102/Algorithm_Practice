bf1b73aac48cbd6191fa1d4ba8f1dfef8f34d14c
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-01-30 21:55:08 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -10,3 +10,11 @@ def solution(lottos, win_nums):
                 prize += 1
                 
     return [rank[prize + unknown], rank[prize]]
+
+'''
+# 집합 연산 사용
+rank = {6:1,5:2,4:3,3:4,2:5,1:6,0:6}
+
+return [rank[len(set(lottos) & set(win_nums)) + lottos.count(0)], rank[len(set(lottos) & set(win_nums))]]
+
+'''
