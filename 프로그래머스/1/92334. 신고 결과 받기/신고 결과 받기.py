69f18ac09d21f42969925532abbc37910c634766
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-02-02 01:02:19 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -16,3 +16,21 @@ def solution(id_list, report, k):
             mail[do] += 1
     
     return list(mail.values())
+
+
+'''
+# 딕셔너리 값 바로 비교
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
+'''
+
