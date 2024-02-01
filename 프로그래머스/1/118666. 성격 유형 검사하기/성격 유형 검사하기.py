b6c8967c7b43635a937fc90121e9a9d25ab954a0
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-02-02 00:17:16 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.33 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -25,3 +25,25 @@ def solution(survey, choices):
         
     
     return res
+
+'''
+# dict 사용하지 않는 방법
+def solution(survey, choices):
+    answer = ''
+    RTCFJMAN = [0,0,0,0,0,0,0,0]
+    str = "RTCFJMAN"
+    for i in range(len(survey)):
+        RTCFJMAN[str.index(survey[i][1])] += choices[i]-4
+
+    if(RTCFJMAN[0]>=RTCFJMAN[1]): answer+= "R"
+    else: answer+="T"
+    if(RTCFJMAN[2]>=RTCFJMAN[3]): answer+= "C"
+    else: answer+="F"
+    if(RTCFJMAN[4]>=RTCFJMAN[5]): answer+= "J"
+    else: answer+="M"
+    if(RTCFJMAN[6]>=RTCFJMAN[7]): answer+= "A"
+    else: answer+="N"
+
+
+    return answer
+'''
