84c204d29cc83031b25a86e4b297460f01a8d6c5
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-02-09 17:29:35 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.68 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -14,3 +14,24 @@ def solution(s):
         s = res
         cnt += 1
     return [cnt, zero]
+
+'''
+# 0을 제거한 문자열의 길이 = 1의 개수
+def solution(s):
+    zero = 0
+    cnt = 0
+    while s != '1':
+    
+        zero += s.count('0')
+        c = s.count('1')
+        
+        res = ''
+        while c > 0:
+            d = c % 2
+            c = c // 2
+            res = str(d) + res
+        
+        s = res
+        cnt += 1
+    return [cnt, zero]
+'''
