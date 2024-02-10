35cc4f4295275311dabad697481b1e108dc40ac2
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-02-10 18:16:47 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -13,3 +13,13 @@ def solution(n):
     for i in range(n+1, 1000001):
         if get_ones(n) == get_ones(i):
             return i
+'''
+# 이진수 구하는 함수 bin
+def solution(n):
+    num1 = bin(n).count('1')
+    while True:
+        n = n + 1
+        if num1 == bin(n).count('1'):
+            break
+    return n
+'''
