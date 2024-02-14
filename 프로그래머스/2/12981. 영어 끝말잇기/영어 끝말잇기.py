591dfd8d4885d2e86ed91c3c5b18d5b06837eabb
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-02-14 23:20:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.17 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,17 @@
+def solution(n, words):
+    cnt = [0]*n
+    idx = 0
+    for i, word in enumerate(words):
+        idx = i % n
+        cnt[idx] += 1
+        
+        if word in words[:i] or (i > 0 and word[0] != words[i-1][-1]):
+            return [idx+1, cnt[idx]]
+    
+    return [0,0]
+        
+        
+        
+        
+
+    return answer
