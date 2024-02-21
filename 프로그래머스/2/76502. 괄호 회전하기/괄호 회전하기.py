e0de69423197b2f70eb3559175f4aadda7170e76
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-02-21 20:03:14 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,27 +1,21 @@
 def solution(s):
     ln = len(s)
-    s_2 = s*2
     cnt = 0
     match = {')':'(', '}':'{', ']':'['}
     
     for i in range(ln):
         stack = []
-        for j in s_2[i:i+ln]:
+        for j in range(i, i+ln):
+            c = s[j%ln]
+            if c in '({[':
+                stack.append(c)
+            elif c in ')}]':
+                if stack and stack[-1] == match[c]:
+                    stack.pop()
+                else:
+                    break
+        else:           
             if not stack:
-                stack.append(j)
-                continue
-            
-            if j in '({[':
-                stack.append(j)
-                continue
-                
-            if j in ']})' and stack[-1] == match[j]:
-                stack.pop()
-                continue
-            else:
-                stack.append(j)
-                continue
-        if not stack:
-            cnt += 1
+                cnt += 1
     
     return cnt
