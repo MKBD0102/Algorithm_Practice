a8a7a47a9ccb346f37d45032cbf3ed9bfa54cf99
Jihyeon Min <110018668+MKBD0102@users.noreply.github.com>
2024-02-23 23:40:42 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -11,3 +11,13 @@ def solution(citations):
             max_h = i
     
     return max_h
+
+'''
+def solution(citations):
+    citations = sorted(citations)
+    l = len(citations)
+    for i in range(l):
+        if citations[i] >= l-i:
+            return l-i
+    return 0
+'''
